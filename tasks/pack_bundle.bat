7b34639f8f4ff9d2892a06e73876bfc09c3bf142
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2021-12-28 13:47:56 +0100

    fix: wcc cooking

--- tasks/pack_bundle.bat
+++ tasks/pack_bundle.bat
@@ -5,6 +5,10 @@ set PACKED_DLC_CONTENT=%PACKED%\DLC\dlc%MOD_NAME%\content
 
 cd %WCC_DIR%
 
+:: cook
+del %DLC_BUNDLE%\cook.db
+%WCC% cook -platform="pc" -mod=%DLC_TEXTURE_CACHE% -basedir=%DLC_TEXTURE_CACHE% -outdir=%DLC_BUNDLE%
+
 :: pack
 %WCC% pack -dir=%DLC_BUNDLE% -outdir=%PACKED_DLC_CONTENT%
 
