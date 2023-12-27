295ed242c431cdeddc8fefd950554167db6505ce
SpontanCombust <cedro_p@o2.pl>
2023-12-27 22:10:04 +0100

    move and rename local scripts files

--- tasks/parse_menu.bat
+++ tasks/parse_menu.bat
@@ -3,4 +3,4 @@ call .\tasks\variables.bat
 
 echo Parsing menu using Mod Settings Framework parser...
 
-%MSF_PARSER% "%MOD_MENUS%\ImmersiveBeardTrimming.xml" -o "%MOD_SCRIPTS%\local\ImmersiveBeardTrimmingSettings.ws"
+%MSF_PARSER% "%MOD_MENUS%\ImmersiveBeardTrimming.xml" -o "%MOD_SCRIPTS%\local\immersive_beard_trimming\settings.ws"
